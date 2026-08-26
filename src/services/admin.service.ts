74ad7776d3f6d8bc42f85082f760804a2b403320
saurabh garkoti <saurabhgarkotiii@gmail.com>
2026-08-26 23:48:14 +0530

    formatting changed

--- src/services/admin.service.ts
+++ src/services/admin.service.ts
@@ -1,50 +1,35 @@
-import prisma from "@/lib/prisma"
-import type { Prisma } from "@prisma/client"
-
- 
-
-
-
-
-
-
-
-
+import prisma from "@/lib/prisma";
+import type { Prisma } from "@prisma/client";
 
 export async function createMemeTemplate(data: Prisma.MemeTemplateCreateInput) {
-
-    return prisma.memeTemplate.create({data})
+  return prisma.memeTemplate.create({ data });
 }
 
-export async function createMemeTemplates(data: Prisma.MemeTemplateCreateManyInput[]) {
-    return prisma.memeTemplate.createMany({ data, skipDuplicates: true })
+export async function createMemeTemplates(
+  data: Prisma.MemeTemplateCreateManyInput[],
+) {
+  return prisma.memeTemplate.createMany({ data, skipDuplicates: true });
 }
 
-export async function updateMemeTemplate(id: string, data: Prisma.MemeTemplateUpdateInput) {
- 
-return prisma.memeTemplate.update({
-    where: {id},
-    data
-})
-
+export async function updateMemeTemplate(
+  id: string,
+  data: Prisma.MemeTemplateUpdateInput,
+) {
+  return prisma.memeTemplate.update({
+    where: { id },
+    data,
+  });
 }
 
-export async function deleteMemeTemplate(id: string){
-
-    return prisma.memeTemplate.delete({
-        where: {id}
-    })
+export async function deleteMemeTemplate(id: string) {
+  return prisma.memeTemplate.delete({
+    where: { id },
+  });
 }
 
-export async function toggleMemeSelection(id: string, enabled: boolean){
-
-     return prisma.memeTemplate.update({
+export async function toggleMemeSelection(id: string, enabled: boolean) {
+  return prisma.memeTemplate.update({
     where: { id },
     data: { selectionEnabled: enabled },
-  })
+  });
 }
-
-
-
-
-
